d2c4e876c148aaefb69cd6b7c9800ae6b0526267
empirefoxit <empirefoxit@gmail.com>
2019-04-29 23:08:15 +0800

    update plural

--- skipper.go
+++ skipper.go
@@ -15,24 +15,12 @@ type PluralSkipper struct {
 	m       map[string]string
 }
 
-var othersMap map[string]bool
-
-func getOthersMap() map[string]bool {
-	if othersMap == nil {
-		othersMap = make(map[string]bool, len(plural.Others))
-		for _, c := range plural.Others {
-			othersMap[c] = true
-		}
-	}
-	return othersMap
-}
-
 func NewPluralSkipper(culture string, ordinal bool) (*PluralSkipper, error) {
-	if getOthersMap()[culture] {
+	if plural.IsOthers(culture) {
 		return &PluralSkipper{skipAll: true}, nil
 	}
 
-	c, ok := plural.Cultures[culture]
+	c, ok := plural.CulturesMap()[culture]
 	if !ok {
 		return nil, fmt.Errorf("culture name not found from plural.Cultures: %s", culture)
 	}
